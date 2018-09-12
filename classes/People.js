a2110a6672b8db7174c2005afc04fb4de8952128
Dufaure Romain <contact@romain-dufaure.fr>
2018-09-12 14:32:31 +0200

    add classes file + People.js CLASS

---
+++ classes/People.js
@@ -0,0 +1,19 @@
+var Peoples = {
+    //controller
+    people: function(id, lastName, firstName, phone, city, postalCode, address)
+	{
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phone = phone;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.address = address;
+    },
+    //méthodes
+    nameFucntion: function()
+	{
+        /* à remplir */
+        return info;
+    }
+};
