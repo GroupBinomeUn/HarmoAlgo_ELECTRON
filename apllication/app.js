543f9819b05ab70fa8da26abb81e6d66f5ca712b
Dufaure Romain <contact@romain-dufaure.fr>
2018-09-12 15:23:46 +0200

    update classe and modules + add application source codes

---
+++ apllication/app.js
@@ -0,0 +1,7 @@
+//import * as PeopleModule from "../classes/People.js";
+
+var lePeople = new Peoples();
+
+lePeople.setFirstName = "François";
+
+alert(lePeople.getFirstName);
--- classes/People.js
+++ classes/People.js
@@ -1,7 +1,6 @@
-var Peoples = {
-    //controller
-    people: function(id, lastName, firstName, phone, city, postalCode, address)
-	{
+class Peoples {
+    //constructeur
+    constructor(id, lastName, firstName, phone, city, postalCode, address) {
         this.id = id;
         this.lastName = lastName;
         this.firstName = firstName;
@@ -9,11 +8,55 @@ var Peoples = {
         this.city = city;
         this.postalCode = postalCode;
         this.address = address;
-    },
-    //méthodes
-    nameFucntion: function()
-	{
-        /* à remplir */
-        return info;
     }
-};
+
+    //méthodes
+    get getId(){
+        return this.id();
+    }
+    set setId(id){
+        this.id = id;
+    }
+
+    get getLastName(){
+        return this.lastName;
+    }
+    set setLastName(lastName){
+        this.lastName = lastName;
+    }
+
+    get getFirstName(){
+        return this.firstName;
+    }
+    set setFirstName(firstName){
+        this.firstName = firstName;
+    }
+
+    get getPhone(){
+        return this.phone;
+    }
+    set setPhone(phone){
+        this.phone = phone;
+    }
+
+    get getCity(){
+        return this.city;
+    }
+    set setCity(city){
+        this.city = city;
+    }
+
+    get getPostalCode(){
+        return this.postalCode;
+    }
+    set setPostalCode(postalCode){
+        this.postalCode = postalCode;
+    }
+
+    get getAddress(){
+        return this.address;
+    }
+    set setPostalCode(address){
+        this.address = address;
+    }
+}
