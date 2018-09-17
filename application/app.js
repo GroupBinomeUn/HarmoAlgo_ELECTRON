a463bde9c968809532f8bbcb2357cd8628f06f73
Magical Unicorn <mailto.magicalunicorn@gmail.com>
2018-09-17 11:52:04 +0200

    Modifications

--- application/app.js
+++ application/app.js
@@ -2,8 +2,6 @@
 
 var lePeople = new Peoples();
 
-var listContact = [];
-
 var lePeople = new Peoples("1", "George", "Pompidou", "0554684548", "Paris", "75000", "15 rue des Mimosa");
 var lePeople2 = new Peoples("2", "Dumas", "François", "05554548754", "Paris", "75000", "5 rue des Mimosa");
 var lePeople3 = new Peoples("3", "Jean-Paul", "Rustre", "0555445875", "Paris", "75000", "2 rue des Mimosa");
@@ -15,4 +13,4 @@ var lePeople8 = new Peoples("8", "Kévin", "Dupont", "0554215487", "Paris", "750
 var lePeople9 = new Peoples("9", "Carine", "Fougare", "0545487544", "Paris", "75000", "28 rue des Mimosa");
 var lePeople10 = new Peoples("10", "Eric", "Montier", "0102030405", "Paris", "75000", "12 rue des Mimosa");
 
-listContact.push(lePeople, lePeople2, lePeople3, lePeople4, lePeople5, lePeople6, lePeople7, lePeople8, lePeople9, lePeople10);
+listPeoples.push(lePeople, lePeople2, lePeople3, lePeople4, lePeople5, lePeople6, lePeople7, lePeople8, lePeople9, lePeople10);
--- renderer.js
+++ renderer.js
@@ -4,12 +4,40 @@
 const {dialog} = require('electron').remote;
 
 function setContact() {
-    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+	var lastName = document.getElementById('lastName').value;
+	var firstName = document.getElementById('firstName').value;
+	var phone = document.getElementById('phone').value;
+	var city = document.getElementById('city').value;
+	var postalCode = document.getElementById('postalCode').value;
+	var address = document.getElementById('address').value;
+	var thePeople = new Peoples("", lastName, firstName, phone, city, postalCode, address);
+	listPeoples.push(thePeople);
+	document.querySelector('#dialog').style.display = "none";
+	viewPeoples();
+}
+
+function viewPeoples() {
+	if(listPeoples.length >= 1) {
+		var temp = "<tr><th>ID</th><th>Nom</th><th>Prenom</th><th>Téléphone</th><th>Ville</th><th>Code postal</th><th>Adresse</th><th></th></tr>";
+		for(var index in listPeoples) { 
+			if (listPeoples.hasOwnProperty(index)) {
+				var btnDel = '<a href="#" title="Supprimer" ><svg class="icon icon-bin"><use xlink:href="#icon-bin"></use></svg></a>';
+				temp += "<tr><td>" + listPeoples[index].getId +  "</td><td>" + listPeoples[index].getLastName + "</td><td>" + listPeoples[index].getFirstName + "</td><td>" + listPeoples[index].getPhone + "</td><td>" + listPeoples[index].getCity + "</td><td>" + listPeoples[index].getPostalCode + "</td><td>" + listPeoples[index].getAddress + "</td><td>" + btnDel + "</td></tr>";
+			}
+		}
+		document.querySelector('#list_Peoples').innerHTML = temp;
+	}
 }
 
 function addContact() {
-    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+    document.querySelector('#dialog').style.display = "block";
 }
 
+function closeDialog() {
+	document.querySelector('#dialog').style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', viewPeoples);
 document.querySelector('#addContact').addEventListener('click', addContact);
 document.querySelector('#submit').addEventListener('click', setContact);
+document.querySelector('#close').addEventListener('click', closeDialog);
