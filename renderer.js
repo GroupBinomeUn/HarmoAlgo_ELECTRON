b8903e9c4746b2a1d2266cb8af58e8ac29a97a5a
Dufaure Romain <dufetude@hotmail.fr>
2018-09-17 21:32:03 +0200

    update app

--- renderer.js
+++ renderer.js
@@ -3,13 +3,37 @@
 // All of the Node.js APIs are available in this process.
 const {dialog} = require('electron').remote;
 
+//dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+
 function setContact() {
-    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+    /* à terminer */
 }
 
 function addContact() {
+    document.querySelector('#dialog').style.display = "block";
+}
+
+function closeDialog() {
+    document.querySelector('#dialog').style.display = "none";
+}
+
+function loadFile(){
+    /* à terminer */
     dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+}
+
+function saveFile(){
+    /* à terminer */
+    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+}
+
+function closeApp(){
+    window.close();
 }
 
 document.querySelector('#addContact').addEventListener('click', addContact);
 document.querySelector('#submit').addEventListener('click', setContact);
+document.querySelector('#close').addEventListener('click', closeDialog);
+document.querySelector('#close_app').addEventListener('click', closeApp);
+document.querySelector('#load_file').addEventListener('click', loadFile);
+document.querySelector('#save_file').addEventListener('click', saveFile);
