9d22e1d6ab968bd30d53b844536f8ffbe9dfd55a
Dufaure Romain <dufetude@hotmail.fr>
2018-09-17 10:42:22 +0200

    update app

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {dialog} = require('electron').remote;
+
+function setContact() {
+    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+}
+
+function addContact() {
+    dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
+}
+
+document.querySelector('#addContact').addEventListener('click', addContact);
+document.querySelector('#submit').addEventListener('click', setContact);
