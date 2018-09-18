985c8f1cffdc4f39cdfc10edc3564523db11f35d
Dufaure Romain <dufetude@hotmail.fr>
2018-09-18 17:41:54 +0200

    update app

--- renderer.js
+++ renderer.js
@@ -14,30 +14,62 @@ function setContact() {
 	var address = document.getElementById('address').value;
 	var thePeople = new Peoples(lastId++, lastName, firstName, phone, city, postalCode, address);
 	listPeoples.push(thePeople);
-	document.querySelector('#dialog').style.display = "none";
-	viewPeoples();
+	document.querySelector('.dialog').style.display = "none";
+	
+	document.querySelector('#list_Peoples').innerHTML = viewPeoples('table');
 }
 
-function viewPeoples() {
+function update(){	
+	document.querySelector('#list_Peoples').innerHTML = viewPeoples('table');
+	document.querySelector('#list-contact-to-delete').innerHTML = viewPeoples('select');
+}
+
+function viewPeoples(type) {
+	var i = 0;
 	if(listPeoples.length >= 1) {
 		var temp = "<tr><th>ID</th><th>Nom</th><th>Prenom</th><th>Téléphone</th><th>Ville</th><th>Code postal</th><th>Adresse</th><th></th></tr>";
 		for(var index in listPeoples) { 
 			if (listPeoples.hasOwnProperty(index)) {
-				var btnDel = '<svg title="Supprimer" class="icon icon-bin bin"><use xlink:href="#icon-bin"></use></svg>';
-				temp += "<tr><td>" + listPeoples[index].getId +  "</td><td>" + listPeoples[index].getLastName + "</td><td>" + listPeoples[index].getFirstName + "</td><td>" + listPeoples[index].getPhone + "</td><td>" + listPeoples[index].getCity + "</td><td>" + listPeoples[index].getPostalCode + "</td><td>" + listPeoples[index].getAddress + "</td><td>" + btnDel + "</td></tr>";
+				if(type == 'select'){
+					temp += "<option value='" + listPeoples[index].getId + "' >" + listPeoples[index].getLastName + " " + listPeoples[index].getFirstName + " " + listPeoples[index].getPhone + " " + listPeoples[index].getCity + " " + listPeoples[index].getPostalCode + " " + listPeoples[index].getAddress + "</option>";
+				}
+				else{
+					var btnDel = '<svg title="Supprimer" class="icon icon-bin bin"><use xlink:href="#icon-bin"></use></svg>';
+					if(i%2 == 1){
+						temp += "<tr class='tr__pair' ><td>" + listPeoples[index].getId +  "</td><td>" + listPeoples[index].getLastName + "</td><td>" + listPeoples[index].getFirstName + "</td><td>" + listPeoples[index].getPhone + "</td><td>" + listPeoples[index].getCity + "</td><td>" + listPeoples[index].getPostalCode + "</td><td>" + listPeoples[index].getAddress + "</td><td>" + btnDel + "</td></tr>";
+					}
+					else{
+						temp += "<tr><td>" + listPeoples[index].getId +  "</td><td>" + listPeoples[index].getLastName + "</td><td>" + listPeoples[index].getFirstName + "</td><td>" + listPeoples[index].getPhone + "</td><td>" + listPeoples[index].getCity + "</td><td>" + listPeoples[index].getPostalCode + "</td><td>" + listPeoples[index].getAddress + "</td><td>" + btnDel + "</td></tr>";
+					}
+					i++;
+				}
 			}
 		}
-		document.querySelector('#list_Peoples').innerHTML = temp;
+		return temp;
 	}
 }
 
-function addContact() {
-	document.querySelector('#dialog').style.display = "block";
-	document.querySelector('#list_Peoples').scrollTop;
+function displayDialogAddContact() {
+	document.querySelector('#dialog-add-contact').style.display = "block";
+	document.querySelector('#list').scrollIntoView({
+		behavior: 'smooth'
+	});
 }
 
-function closeDialog() {
-    document.querySelector('#dialog').style.display = "none";
+function displayDialogDelContact() {
+	document.querySelector('#list-contact-to-delete').innerHTML = viewPeoples('select');
+	document.querySelector('#dialog-del-contact').style.display = "block";
+	document.querySelector('#list').scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
+function closeDialogAddContact() {
+    document.querySelector('#dialog-add-contact').style.display = "none";
+}
+
+function closeDialogDelContact() {
+    document.querySelector('#dialog-del-contact').style.display = "none";
 }
 
 function loadFile(){
@@ -50,15 +82,49 @@ function saveFile(){
     dialog.showErrorBox('Erreur !', 'L\'application a rencontré une erreur. Votre ordinateur va s\'auto-détruire dans 10 secondes.');
 }
 
+function search(){
+	var txt = document.querySelector('#txt-search');
+	var type = document.querySelector('#list-search');
+
+	txt.classList.remove('txt-search-error');
+	txt.classList.add('txt-search-good');
+
+	if(txt.value.trim() != ""){
+		dialog.showMessageBox({ message: 'Vous effectuez une recherche sur le mot ' + txt.value + ' de type ' + type.options[type.selectedIndex].text, buttons: ["OK"] });
+	}
+	else{
+		txt.focus();	
+		txt.classList.add('txt-search-error');
+		txt.classList.remove('txt-search-good');
+	}
+}
+
+function select_search(){
+	document.querySelector('#txt-search').focus();
+	document.querySelector('#nav').scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
 function closeApp(){
     window.close();
 }
 
-document.addEventListener('DOMContentLoaded', viewPeoples);
-document.querySelector('#addContact').addEventListener('click', addContact);
-document.querySelector('#menu_addContact').addEventListener('click', addContact);
+document.addEventListener('DOMContentLoaded', update());
 document.querySelector('#submit').addEventListener('click', setContact);
-document.querySelector('#close').addEventListener('click', closeDialog);
 document.querySelector('#close_app').addEventListener('click', closeApp);
 document.querySelector('#load_file').addEventListener('click', loadFile);
 document.querySelector('#save_file').addEventListener('click', saveFile);
+
+//add contact
+document.querySelector('#addContact').addEventListener('click', displayDialogAddContact);
+document.querySelector('#close-add-dialog').addEventListener('click', closeDialogAddContact);
+document.querySelector('#menu_addContact').addEventListener('click', displayDialogAddContact);
+
+//del contact
+document.querySelector('#del_contact').addEventListener('click', displayDialogDelContact);
+document.querySelector('#close-del-dialog').addEventListener('click', closeDialogDelContact);
+
+//recherche
+document.querySelector('#sub-search').addEventListener('click', search);
+document.querySelector('#serach_list').addEventListener('click', select_search);
